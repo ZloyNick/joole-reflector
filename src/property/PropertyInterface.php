3745ddda4b7cd8611c7dd0187694c16386d6e80a
ZloyNick <sitdikov_r@magnit.ru>
2022-03-05 17:48:58 +0500

    2.1.5 release.
    [Property.php] Added new methods: hasDefaultValue(), getDefaultValue(), getProperty(), haveValue(), getVisibility();
    [Property.php] Rewrote methods: getValue();
    [Property.php] Refactoring: $owner is now the object that was passed by reflection;
    
    [ReflectedObject] Refactor: method init() some changes;
    
    [Reflector] Refactor: logic;
    
    [test.cmd] Removed;

--- src/property/PropertyInterface.php
+++ src/property/PropertyInterface.php
@@ -11,11 +11,28 @@ interface PropertyInterface
 {
 
     /**
-     * Sets property accessible.
-     *
-     * @param true|false $value
+     * A property visibilities as integer.
      */
-    public function setAccessible(bool $value): void;
+    public const T_PUBLIC = 0;
+    public const T_PROTECTED = 1;
+    public const T_PRIVATE = 2;
+
+    /**
+     * A property visibilities as string.
+     */
+    public const NAME_T_PUBLIC = 'public';
+    public const NAME_T_PROTECTED = 'protected';
+    public const NAME_T_PRIVATE = 'private';
+
+    /**
+     * Returns property's visibility.
+     *
+     * @param bool $asString If contains true, returns visibility as string,
+     * else returns it as int.
+     * @see PropertyInterface::NAME_T_PROTECTED, PropertyInterface::NAME_T_PROTECTED, PropertyInterface::NAME_T_PROTECTED
+     * @see PropertyInterface::T_PROTECTED, PropertyInterface::T_PROTECTED, PropertyInterface::T_PROTECTED
+     */
+    public function getVisibility(bool $asString = true): string|int;
 
     /**
      * Sets property's value.
